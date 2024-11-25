d72390146068374c8c85938cda25189ca5781718
enricodelazzari <enrico.delazzari@maize.io>
2024-11-25 22:39:24 +0100

    wip

--- config/xml-to-array.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for EnricoDeLazzari/XmlToArray
-return [
-
-];
--- src/XmlToArrayServiceProvider.php
+++ src/XmlToArrayServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace EnricoDeLazzari\XmlToArray;
 
-use EnricoDeLazzari\XmlToArray\Commands\XmlToArrayCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,6 @@ class XmlToArrayServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-xml-to-array')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_xml_to_array_table')
-            ->hasCommand(XmlToArrayCommand::class);
+        $package->name('laravel-xml-to-array');
     }
 }
